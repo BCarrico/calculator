1e8ac63c5ff88ff412b659b1abab98e1690dbc66
BCarrico <BCarrico89@gmail.com>
2022-06-27 13:41:34 -0700

    event listeners functionable for all calculator buttons

---
+++ script.js
@@ -0,0 +1,83 @@
+
+
+// Event Listeners for Buttons //
+
+const clearBtn = document.getElementById('clear');
+clearBtn.addEventListener('click', () => {
+    console.log("clear clicked");
+}) 
+
+const backBtn = document.getElementById('backspace');
+backBtn.addEventListener('click', () => {
+    console.log("back clicked");
+}) 
+
+const divideBtn = document.getElementById('divide');
+divideBtn.addEventListener('click', () => {
+    console.log("divide clicked");
+}) 
+
+const multiplyBtn = document.getElementById('multiply');
+multiplyBtn.addEventListener('click', () => {
+    console.log("X clicked");
+}) 
+
+const addBtn = document.getElementById('add');
+addBtn.addEventListener('click', () => {
+    console.log("+ clicked");
+}) 
+
+const subtractBtn = document.getElementById('subtract');
+subtractBtn.addEventListener('click', () => {
+    console.log("- clicked");
+}) 
+
+const zeroBtn = document.getElementById('0');
+zeroBtn.addEventListener('click', () => {
+    console.log("0 clicked");
+}) 
+
+const oneBtn = document.getElementById('1');
+oneBtn.addEventListener('click', () => {
+    console.log("1 clicked");
+}) 
+
+const twoBtn = document.getElementById('2');
+twoBtn.addEventListener('click', () => {
+    console.log("2 clicked");
+}) 
+
+const threeBtn = document.getElementById('3');
+threeBtn.addEventListener('click', () => {
+    console.log("3 clicked");
+}) 
+
+const fourBtn = document.getElementById('4');
+fourBtn.addEventListener('click', () => {
+    console.log("4 clicked");
+}) 
+
+const fiveBtn = document.getElementById('5');
+fiveBtn.addEventListener('click', () => {
+    console.log("5 clicked");
+}) 
+
+const sixBtn = document.getElementById('6');
+sixBtn.addEventListener('click', () => {
+    console.log("6 clicked");
+}) 
+
+const sevenBtn = document.getElementById('7');
+sevenBtn.addEventListener('click', () => {
+    console.log("7 clicked");
+}) 
+
+const eightBtn = document.getElementById('8');
+eightBtn.addEventListener('click', () => {
+    console.log("8 clicked");
+}) 
+
+const nineBtn = document.getElementById('9');
+nineBtn.addEventListener('click', () => {
+    console.log("9 clicked");
+}) 
