9828c1e8d109ed2dbc86ed526e913cdba2ef6290
BCarrico <BCarrico89@gmail.com>
2022-06-27 15:42:43 -0700

    Change to event listener and HTML setup. Able to click and display number in display

--- script.js
+++ script.js
@@ -1,83 +1,64 @@
+const calculator = document.querySelector('.calculator')
+const keys = calculator.querySelector('.calculatorKeys')
+const display = document.querySelector('.calculatorDisplay')
+
+keys.addEventListener('click', e => {
+    if (e.target.matches('button')) {
+        const key = e.target;
+        const action = key.dataset.action;
+        if (!action) {
+            console.log('number key')
+        } else if (
+            action === 'add' ||
+            action === 'subtract' ||
+            action === 'multiply' ||
+            action === 'divide'
+        ) {
+            console.log('operator key!')
+        } else if (
+            action === 'decimal' 
+        ) {
+            console.log('decimal key!')
+        } else if (
+            action === 'clear'
+        ) {
+            console.log('clear key!')
+        } else if (
+            action === 'backspace'
+        ) {
+            console.log('backspace key!')
+        } else if (
+            action === 'calculate'
+        ) {
+            console.log('calculate key')
+        } else {
+            console.log('error!!')
+        }
+        
+    }
+})
+
+keys.addEventListener('click', e => {
+    if (e.target.matches('button')){
+        const key = e.target;
+        const action = key.dataset.action;
+        const keyContent = key.textContent;
+        const displayedNum = display.textContent;
+
+        if (!action){
+            if (displayedNum === '0'){
+                display.textContent = keyContent;
+            }
+        }
+    
+        if (!action){
+            if (displayedNum === '0') {
+                display.textContent = keyContent;
+            } else {
+                display.textContent = displayedNum + keyContent;
+            }
+        }
+    }
+})
 
 
-// Event Listeners for Buttons //
-
-const clearBtn = document.getElementById('clear');
-clearBtn.addEventListener('click', () => {
-    console.log("clear clicked");
-}) 
-
-const backBtn = document.getElementById('backspace');
-backBtn.addEventListener('click', () => {
-    console.log("back clicked");
-}) 
-
-const divideBtn = document.getElementById('divide');
-divideBtn.addEventListener('click', () => {
-    console.log("divide clicked");
-}) 
-
-const multiplyBtn = document.getElementById('multiply');
-multiplyBtn.addEventListener('click', () => {
-    console.log("X clicked");
-}) 
-
-const addBtn = document.getElementById('add');
-addBtn.addEventListener('click', () => {
-    console.log("+ clicked");
-}) 
-
-const subtractBtn = document.getElementById('subtract');
-subtractBtn.addEventListener('click', () => {
-    console.log("- clicked");
-}) 
-
-const zeroBtn = document.getElementById('0');
-zeroBtn.addEventListener('click', () => {
-    console.log("0 clicked");
-}) 
-
-const oneBtn = document.getElementById('1');
-oneBtn.addEventListener('click', () => {
-    console.log("1 clicked");
-}) 
-
-const twoBtn = document.getElementById('2');
-twoBtn.addEventListener('click', () => {
-    console.log("2 clicked");
-}) 
-
-const threeBtn = document.getElementById('3');
-threeBtn.addEventListener('click', () => {
-    console.log("3 clicked");
-}) 
-
-const fourBtn = document.getElementById('4');
-fourBtn.addEventListener('click', () => {
-    console.log("4 clicked");
-}) 
-
-const fiveBtn = document.getElementById('5');
-fiveBtn.addEventListener('click', () => {
-    console.log("5 clicked");
-}) 
-
-const sixBtn = document.getElementById('6');
-sixBtn.addEventListener('click', () => {
-    console.log("6 clicked");
-}) 
-
-const sevenBtn = document.getElementById('7');
-sevenBtn.addEventListener('click', () => {
-    console.log("7 clicked");
-}) 
-
-const eightBtn = document.getElementById('8');
-eightBtn.addEventListener('click', () => {
-    console.log("8 clicked");
-}) 
-
-const nineBtn = document.getElementById('9');
-nineBtn.addEventListener('click', () => {
-    console.log("9 clicked");
-}) 
